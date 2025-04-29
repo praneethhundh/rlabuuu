b921b04cf7f3da0f4eb775f2231b5e9bf717a114
praneethhundh <130727307+praneethhundh@users.noreply.github.com>
2025-04-29 07:51:47 +0530

    10.R

--- 10.R
+++ 10.R
@@ -2,17 +2,9 @@
 install.packages(c("arules", "arulesViz"), dependencies = TRUE)
 library(arules)
 library(arulesViz)
-
-# Step 2: Load Example Data (Groceries)
 data("Groceries")
-
-# Step 3: Generate Association Rules using Apriori
 rules <- apriori(Groceries, parameter = list(supp = 0.01, conf = 0.3, minlen = 2))
-
-# Step 4: View Summary and Top Rules
 summary(rules) 
 inspect(sort(rules, by = "lift")[1:10])
-
-# Step 5: Visualize Rules
 plot(rules, method = "graph", engine = "htmlwidget")
 plot(rules, method = "grouped")
