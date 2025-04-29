aff6724ff24df6d1e52b41bb4c4a90c764bbfc22
praneethhundh <130727307+praneethhundh@users.noreply.github.com>
2025-04-29 07:55:44 +0530

    7.R

--- 7.R
+++ 7.R
@@ -1,19 +1,10 @@
-# Step 1: Load the iris dataset
+
 data(iris)
-
-# Step 2: View the structure of the dataset
 str(iris)
-
-# Step 3: Perform Simple Linear Regression
 model <- lm(Petal.Length ~ Sepal.Length, data = iris)
-
-# Step 4: View summary of the model
 summary(model)
-
-# Step 5: Plot the data and regression line using ggplot2
-install.packages("ggplot2")  # Only run if ggplot2 isn't installed
+install.packages("ggplot2") 
 library(ggplot2)
-
 ggplot(iris, aes(x = Sepal.Length, y = Petal.Length)) +
   geom_point(color = "darkgreen") +
   geom_smooth(method = "lm", col = "blue", se = FALSE) +
